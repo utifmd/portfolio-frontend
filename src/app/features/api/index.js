722496a9b5604a7ae35e20a5eefd570e1e517227
Utif Milkedori <utifmd@gmail.com>
2021-11-07 23:36:10 +0700

    oldies backend restored

--- src/app/features/api/index.js
+++ src/app/features/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import { scholars, projects } from '../../../domain'
 
-const url = "https://pages-backend.herokuapp.com" // "http://127.0.0.1:5000/" //"https://portfolio-utifmd.herokuapp.com/"
+const url = // "https://pages-backend.herokuapp.com" // "http://127.0.0.1:5000/" 
+    "https://portfolio-utifmd.herokuapp.com/"
 
 const client = axios.create({baseURL: url, timeout: 20000})
 
